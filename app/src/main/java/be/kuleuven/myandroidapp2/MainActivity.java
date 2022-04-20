8b8bcd3907b163c38f70aede9fb24cad1ed90078
MSIIOIO28\bolog <iole.bolognesi@student.kuleuven.be>
2022-04-20 16:33:45 +0100

    first commit

---
+++ app/src/main/java/be/kuleuven/myandroidapp2/MainActivity.java
@@ -0,0 +1,34 @@
+package be.kuleuven.myandroidapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onLoginButton_Clicked  (View caller)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        //intent.putExtra("Order",order);
+        startActivity(intent);
+
+    }
+
+    public void obSignUpButton_Clicked  (View caller)
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+       // intent.putExtra("Order",order);
+        startActivity(intent);
+
+    }
+}

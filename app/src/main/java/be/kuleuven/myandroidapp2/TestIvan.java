2344b598a3edf6cef02be54c248acc6c74889224
DESKTOP-O46DQUM\Lenovo <ivandeyanov.ivanov@student.kuleuven.be>
2022-04-27 18:56:09 +0200

    end of lab wedn

---
+++ app/src/main/java/be/kuleuven/myandroidapp2/TestIvan.java
@@ -0,0 +1,17 @@
+package be.kuleuven.myandroidapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TestIvan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ivan);
+    }
+
+
